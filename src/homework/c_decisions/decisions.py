256a09a265f302b4e30a56f60b0cfd2bf7443e68
anglpnc01 <angel.ponce1@g.austincc.edu>
2024-06-17 22:46:13 +0000

    Homework 3 (upd)

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -8,15 +8,15 @@ def get_faculty_rating(ratio):
     result = ''
 
 
-    if (ratio > .9 and ratio <= 1):
+    if (ratio >= .9 and ratio <= 1):
         result = 'Excellent'
-    elif (ratio > .8 and ratio <= .9):
+    elif (ratio >= .8 and ratio <= .9):
         result = 'Very Good'
-    elif (ratio > .7 and ratio <= .8):
+    elif (ratio >= .7 and ratio <= .8):
         result = 'Good'    
-    elif (ratio > .6 and ratio <= .7):
+    elif (ratio >= .6 and ratio <= .7):
         result = 'Needs Improvement'
-    elif (ratio >= 0 and ratio < .6):
+    elif (ratio >= 0 and ratio <= .6):
         result = 'Unacceptable'
 
     else:
