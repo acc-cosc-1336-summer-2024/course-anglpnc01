8f89d3fcc29913064da7586e09a4fa7d90024d0f
anglpnc01 <angel.ponce1@g.austincc.edu>
2024-07-14 07:26:50 +0000

    Homework 6

--- src/homework/i_dictionaries_sets/dictionary.py
+++ src/homework/i_dictionaries_sets/dictionary.py
@@ -1,1 +1,47 @@
-#
+#using a define distance rule
+def difference(char1, char2):
+
+    if char1 == char2:
+        
+        return 0
+    
+    else:
+        return 1
+    
+
+def get_p_distance(list1, list2):
+
+    if len(list1) != len(list2):
+
+        print('DNA strands not the same length')
+
+    L = len(list1)
+    mismatches = sum(L(list1[i], list2[i]) for i in range(L))
+    
+    p_distance = mismatches / L
+    
+    return p_distance
+
+
+def get_p_distance_matrix(list1):
+
+    master_list = [
+ 	
+ ['T','T','T','C','C','A','T','T','T','A'],#list1
+ 	
+ ['G','A','T','T','C','A','T','T','T','C'],#list2
+ 	
+ ['T','T','T','C','C','A','T','T','T','T'],#list3
+ 	
+ ['G','T','T','C','C','A','T','T','T','A']#list4
+ 	
+]
+
+    result_list1_list2 = get_p_distance(master_list[0], master_list[1])
+    print(f'Result is: {result_list1_list2}')
+
+    result_list1_list3 = get_p_distance(master_list[0], master_list[2])
+    print(f'Result is: {result_list1_list3}')
+
+    result_list1_list4 = get_p_distance(master_list[0], master_list[3])
+    print(f'Result is: {result_list1_list4}')
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,16 @@
-#
+import unittest
+
+from src.homework.i_dictionaries_sets.dictionary import get_p_distance, get_p_distance_matrix
+
+
+class Test_Config(unittest.TestCase):
+
+    def test_get_p_distance(self):
+        self.assertEqual(0.4, get_p_distance(['T','T','T','C','C','A','T','T','T','A'], ['G','A','T','T','C','A','T','T','T','C']))
+
+    def test_get_p_distance_matrix(self):
+        taxa_list = [['T','T','T','C','C','A','T','T','T','A'], ['G','A','T','T','C','A','T','T','T','C'], ['T','T','T','C','C','A','T','T','T','T'],['G','T','T','C','C','A','T','T','T','A']]
+
+        self.assertEqual([[0.0, 0.4, 0.1, 0.1], [0.4, 0.0, 0.4, 0.3], [0.1, 0.4, 0.0, 0.2], [0.1, 0.3, 0.2, 0.0]], get_p_distance(taxa_list))
+
+                         
