c64f0d4e71339fb10af1ea1f70b18ad8da689a3e
anglpnc01 <angel.ponce1@g.austincc.edu>
2024-07-27 04:26:12 +0000

    Homework 7

--- src/homework/j_classes/class_a.py
+++ src/homework/j_classes/class_a.py
@@ -3,14 +3,9 @@ import random
 class die:
     
     
-    def __init__(self):
-        # Initialize roll_value with a default value
-        self.roll_value = None
-
+    
     def roll(self):
-        # Generate a random value between 1 and 6 and store it in roll_value
-            self.roll_value = random.randint(1, 6)
-
+        return random.randint(1,6)
 
     def get_rolled_value(self):
 
--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -1,14 +1,15 @@
 import unittest
 
 
-from src.homework.j_classes.class_a import die, roll
+from src.homework.j_classes.class_a import die
 
 
 class Test_Config(unittest.TestCase):
 
-    def test_get_rolled_value(self):
-        
-        roll_value = die.roll()
+    def test_die_rolls_1_to_6(self):
+        Die = die()
 
-        self.assertEqual(True, roll() >= 1)
-        self.assertEqual(True, roll() <= 12)
+        for i in range(0, 12):
+            roll_value = die.roll()
+            self.assertEqual(True, roll_value >= 1)
+            self.assertEqual(True, roll_value <= 6)
