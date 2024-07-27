f90a9062dcb2711656d4f8ee880ad1eb92972885
anglpnc01 <angel.ponce1@g.austincc.edu>
2024-07-27 03:56:48 +0000

    Homework 7 beta.0

--- src/homework/j_classes/class_a.py
+++ src/homework/j_classes/class_a.py
@@ -1,1 +1,22 @@
-#
+import random
+
+class die:
+    
+    
+    def __init__(self):
+        # Initialize roll_value with a default value
+        self.roll_value = None
+
+    def roll(self):
+        # Generate a random value between 1 and 6 and store it in roll_value
+            self.roll_value = random.randint(1, 6)
+
+
+    def get_rolled_value(self):
+
+            return self.roll_value
+        
+    def __str__(self):
+
+            print(f'The rolled value is: {self.roll_value}')
+    
--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -1,1 +1,14 @@
-#
+import unittest
+
+
+from src.homework.j_classes.class_a import die, roll
+
+
+class Test_Config(unittest.TestCase):
+
+    def test_get_rolled_value(self):
+        
+        roll_value = die.roll()
+
+        self.assertEqual(True, roll() >= 1)
+        self.assertEqual(True, roll() <= 12)
